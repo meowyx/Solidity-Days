8320b29c5ca58171d04183c7240be61b9c290f70
meowy <90390363+myaowy@users.noreply.github.com>
2022-04-08 06:08:03 +0600

    Update learning.sol

--- learning.sol
+++ learning.sol
@@ -1,2 +1,14 @@
-//this is the file for solidity
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Comments {
+    struct Comment {
+        uint32 id;
+        string topic;
+        address creator_address;
+        string message;
+        uint created_at;
+    }
+
 
